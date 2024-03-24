9cff08f73f1dbd17f4f4b48ed91d691d00ecd1a0
Uwe Rosner <oscottmusic@posteo.de>
2024-03-24 16:06:04 +0100

    .

--- src/application.c
+++ src/application.c
@@ -215,8 +215,8 @@ BOOL runApplication(Application* pApp)
               "Not all input files");
 
       if(!showEasyRequest(pApp->pIntuiWindow,
-                         "Continue|Show errors",
-                         "Failed to add some of the input files"))
+                          "Continue|Show errors",
+                          "Failed to add some of the input files"))
       {
         printNotifications(pApp->pNotificationsList);
         clearNotificationsExcept(pApp->pNotificationsList,
