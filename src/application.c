c2cf857065bf0a46a0b90b132a14a93aad9a1fc7
Uwe Rosner <oscottmusic@posteo.de>
2024-04-01 18:02:30 +0200

    Added some test code to see how listbrowser node text can be changed

--- src/application.c
+++ src/application.c
@@ -250,13 +250,54 @@ BOOL runApplication(Application* pApp)
   return FALSE;
 }
 
+static char* fakeNewNames[] = {"Abc\0", "Def", "Ghi", NULL};
+
 void handleGadgets(Application* pApp, ULONG result)
 {
+  struct Node* pNode;
+  FileNode* pFileNode;
+  ULONG i = 0;
+
   switch ((result & WMHI_GADGETMASK))
   {
   case GID_BTN_NAME:
     printFileListNewName(pApp->pFileList);
     break;
+  case GID_BTN_NAME_PART:
+    // TODO: Debug only. Changes new names!
+    {
+      SetGadgetAttrs((struct Gadget *) m_ppGadgets[GID_LISTBROWSER],
+                    pApp->pIntuiWindow, 
+                    NULL,
+                    LISTBROWSER_Labels, ~0,
+                    TAG_DONE);
+
+      for(pNode = pApp->pFileList->lh_Head; pNode->ln_Succ; pNode = pNode->ln_Succ)
+      {
+        if(fakeNewNames[i] == NULL)
+        {
+          break;
+        }
+
+        pFileNode = (FileNode*)pNode;
+        strcpy(pFileNode->NewName, fakeNewNames[i]);
+
+        SetListBrowserNodeAttrs(pNode,
+                                LBNA_Column, 1,
+                                  LBNCA_Text, pFileNode->NewName,
+                                TAG_DONE);
+
+        i++;
+      }
+
+      SetGadgetAttrs((struct Gadget *) m_ppGadgets[GID_LISTBROWSER],
+                    pApp->pIntuiWindow, 
+                    NULL,
+                    LISTBROWSER_Labels, (ULONG)pApp->pFileList,
+                    TAG_DONE);
+
+      break;
+    }
   }
 }
 
