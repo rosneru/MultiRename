81bcf5f5be1d0bcc7eed0248786b38f28854fff3
Uwe Rosner <oscottmusic@posteo.de>
2024-04-22 11:39:04 +0200

    Added (still empty) range select requester

---
+++ src/range_select_requester.c
@@ -0,0 +1,47 @@
+#include <classes/window.h>
+#include <gadgets/chooser.h>
+#include <gadgets/layout.h>
+#include <gadgets/listbrowser.h>
+#include <intuition/classusr.h>
+#include <intuition/gadgetclass.h>
+#include <intuition/icclass.h>
+#include <utility/hooks.h>
+#include <workbench/workbench.h>
+
+#include <clib/compiler-specific.h>
+
+#ifdef __clang__
+  #include <clib/alib_protos.h>
+  #include <clib/exec_protos.h>
+  #include <clib/dos_protos.h>
+  #include <clib/intuition_protos.h>
+  #include <clib/button_protos.h>
+  #include <clib/chooser_protos.h>
+  #include <clib/integer_protos.h>
+  #include <clib/label_protos.h>
+  #include <clib/layout_protos.h>
+  #include <clib/listbrowser_protos.h>
+  #include <clib/string_protos.h>
+  #include <clib/window_protos.h>
+#else
+  #include <proto/alib.h>
+  #include <proto/dos.h>
+  #include <proto/exec.h>
+  #include <proto/intuition.h>
+  #include <proto/button.h>
+  #include <proto/chooser.h>
+  #include <proto/integer.h>
+  #include <proto/label.h>
+  #include <proto/layout.h>
+  #include <proto/listbrowser.h>
+  #include <proto/string.h>
+  #include <proto/window.h>
+#endif
+
+
+#include "range_select_requester.h"
+
+
+void openRangeSelectRequester(Application* pApp)
+{
+}
---
+++ src/range_select_requester.h
@@ -0,0 +1,8 @@
+#ifndef RANGE_SELECT_REQUESTER_H
+#define RANGE_SELECT_REQUESTER_H
+
+#include "application.h"
+
+void openRangeSelectRequester(Application* pApp);
+
+#endif
